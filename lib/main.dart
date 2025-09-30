aa4ed4f1d273d22d086843b4d4f4f5f3f2a9c2d2
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-09-30 21:28:22 +0700

    chore: add firebase to flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'HomePage.dart';
 
 void main() async {
-	await dotenv.load();
-	runApp(const MyApp());
+  await dotenv.load();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-	const MyApp({super.key});
+  const MyApp({super.key});
 
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			debugShowCheckedModeBanner: false,
-			title: 'Chat App',
-			home: const ChatHomePage(),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Chat App',
+      home: const ChatHomePage(),
+    );
+  }
 }
-
